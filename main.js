d9289250559c7d77569ff2b37d5fd2114905e869
jinpyojeon <jinpyo.jeon@trincoll.edu>
2017-04-25 17:29:27 -0400

    added model and loader

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
 var scene, camera, renderer;
-var geometry, material, mesh;
 
 init();
-animate();
+
 
 function init() {
-
     scene = new THREE.Scene();
+
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.z = 1000;
 
-    geometry = new THREE.BoxGeometry(200, 200, 200);
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    var light = new THREE.DirectionalLight(0xffffff, 1, 100);
+    light.position.set(0, 1, 0); //default; light shining from top
+    scene.add(light);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xEEEEEE);
+
+    // var manager = new THREE.LoadingManager();
+    // manager.onProgress = function(item, loaded, total) {
+    //     console.log(item, loaded, total);
+    // };
+
+    var loader = new THREE.OBJLoader();
+
+    loader.load('models/Stick_Figure_by_Swp.OBJ',
+        function(object) {
+            object.position.set(0, 0, 0);
+            scene.add(object);
+        }
+    );
 
     window.onload = function() {
         document.body.appendChild(renderer.domElement);
     }
+
+    render();
 }
 
-function animate() {
-
-    requestAnimationFrame(animate);
-
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-
+function render() {
+    requestAnimationFrame(render);
     renderer.render(scene, camera);
-
 }
