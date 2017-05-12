001e24b9161ee00c3993f8eab7905e75249de1f4
jinpyojeon <jinpyo.jeon@trincoll.edu>
2017-05-11 22:25:10 -0400

    new changes

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 var scene, camera, renderer;
-var mousePos = { x: 0, y: 0 };
 
-var player, floor, sun, sky;
+var floor, sun, sky, player; // global variables
 
+// Variables related to games
 var game = {
     finished: false,
     points: 0,
-    speed: 1
+    speed: 1,
+    spawnRate: 1,
+    input: { left: false, right: false }
 };
 
 var newTime = new Date().getTime();
 var oldTime = new Date().getTime();
 
+// Arrays to hold model objects
+var obstacleTypes = [];
 var obstacles = [];
 var coins = [];
 
@@ -22,7 +26,8 @@ var Colors = {
     pink: 0xF5986E,
     brownDark: 0x23190f,
     blue: 0x68c3c0,
-    gold: 0xffdf00
+    gold: 0xffdf00,
+    sunsetOrange: 0xfe5b35
 };
 
 init();
@@ -30,13 +35,15 @@ init();
 function init() {
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 10;
-    camera.position.y = 2;
+    // Initialize camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 20000);
+    camera.position.z = 14;
+    camera.position.y = 4;
 
-    var ambientLight = new THREE.AmbientLight(Colors.white); // soft white light
+    var ambientLight = new THREE.AmbientLight(Colors.white);
     scene.add(ambientLight);
 
+    // Add sun 
     var geomSun = new THREE.TetrahedronGeometry(8, 2);
     var matSun = new THREE.MeshPhongMaterial({
         color: Colors.red,
@@ -45,30 +52,18 @@ function init() {
         shading: THREE.FlatShading
     });
     sun = new THREE.Mesh(geomSun, matSun);
+    sun.position.set(9, 20, -10);
     sun.scale.set(0.25, 0.25, 0.25);
-    sun.position.set(5, 20, -10);
     scene.add(sun);
 
-    var light = new THREE.DirectionalLight(Colors.red, 1);
-    light.position.set(0, 1, 0);
+    // Set Sun as the source of directional light and intensity 
+    // By Eucludian distance
+    var light = new THREE.DirectionalLight(Colors.sunsetOrange,
+        sun.position.distanceTo(new THREE.Vector3(0, 0, 0)));
+    light.position.set(sun.position);
     scene.add(light);
 
-    var listener = new THREE.AudioListener();
-    camera.add(listener);
-
-    // // create a global audio source
-    // var sound = new THREE.Audio(listener);
-
-    // var audioLoader = new THREE.AudioLoader();
-
-    // //Load a sound and set it as the Audio object's buffer
-    // audioLoader.load('sounds/ambient.ogg', function(buffer) {
-    //     sound.setBuffer(buffer);
-    //     sound.setLoop(true);
-    //     sound.setVolume(0.5);
-    //     sound.play();
-    // });
-
+    // Loader to load up the textruers
     var loader = new THREE.TextureLoader();
 
     var grassTexture = loader.load('src/images/grasslight-small.jpg', function(texture) {
@@ -79,7 +74,7 @@ function init() {
 
     floor = new THREE.Mesh(
         new THREE.PlaneGeometry(100, 100000, 8, 8),
-        new THREE.MeshBasicMaterial({
+        new THREE.MeshLambertMaterial({
             map: grassTexture
         }));
     floor.rotation.x -= Math.PI / 2;
@@ -100,101 +95,72 @@ function init() {
     sky.position.z = -60;
     scene.add(sky);
 
-
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.setClearColor(0xEEEEEE);
+    renderer.setSize(window.innerWidth,
+        Math.round(window.innerHeight * 0.8));
 
-    var loader = new THREE.OBJLoader();
+    // Load up the figure for the game
+    var objLoader = new THREE.OBJLoader();
+    var mtlLoader = new THREE.MTLLoader();
+    objLoader.setPath('models/');
+    mtlLoader.setPath('models/');
 
-    // var ratamahatta = new THREEx.MD2CharacterRatmahatta();
-    // scene.add(ratamahatta.character.object3d);
-
-    loader.load('models/Stick_Figure_by_Swp.OBJ',
+    objLoader.load('Stick_Figure_by_Swp.OBJ',
         function(object) {
             object.position.set(1, 0, 8);
             object.scale.set(0.5, 0.5, 0.5);
-            // object.rotation.set(0, -Math.PI / 2, 1);
             player = object;
             scene.add(player);
         }
     );
 
-    // var mtlLoader = new THREE.MTLLoader();
-    // mtlLoader.setPath('models/');
-    // mtlLoader.load('Oak_Dark_01.mtl', function(materials) {
-
-    //     materials.preload();
-
-    //     var objLoader = new THREE.OBJLoader();
-    // loader.setMaterials(materials);
-    // loader.setPath('models');
-    // lader.load('Oak_Dark_01.obj', function(object) {
-
-    //     object.position.y = -95;
-    //     scene.add(object);
-
-    // }, onProgress, onError);
-
-    // });
+    loadObstacleTypes();
 
     window.onload = function() {
         document.body.appendChild(renderer.domElement);
+
+        document.addEventListener('keydown', function(event) {
+            if (event.keyCode === 37) game.input.left = true
+            if (event.keyCode === 39) game.input.right = true
+        });
+
+        document.addEventListener('keyup', function(event) {
+            if (event.keyCode === 37) game.input.left = false
+            if (event.keyCode === 39) game.input.right = false
+        });
+
         render();
     }
-
-    document.addEventListener("mousemove", handleMouseMove, false);
-
-    document.body.addEventListener('keydown', function(event) {
-        var inputs = player.input;
-        if (event.keyCode === 37) inputs.left = true
-        if (event.keyCode === 39) inputs.right = true
-    });
-
-    document.body.addEventListener('keyup', function(event) {
-        var inputs = player.input;
-        if (event.keyCode === 37) inputs.left = false
-        if (event.keyCode === 39) inputs.right = false
-    });
 }
 
+// The main rendering loop function
 function render() {
 
     if (!game.finished) {
-        if (player) player.position.set(mousePos.x * 10, 0, 8);
         floor.position.z += 0.3 * game.speed;
         sky.position.x += 0.1;
-        updateSun();
         procGenerateRocks();
-        // procGenerateCoins();
         moveObstacles();
         moveCoins();
         detectCollision();
+        updatePlayer();
         game.speed += 0.0001;
+        game.spawnRate += 0.001;
         renderer.render(scene, camera);
         requestAnimationFrame(render);
     }
 
 }
 
-function updateSun() {
-    if (sun) {
-        sun.rotation.z += Math.sin(0.1);
-        sun.rotation.y += Math.sin(0.1);
-        sun.position.y *= Math.PI * Math.sin(sun.position.y);
-    }
-}
-
-function handleMouseMove(event) {
-    var tx = -1 + (event.clientX / window.innerWidth) * 2;
-    var ty = 1 - (event.clientY / window.innerHeight) * 2;
-    mousePos = { x: tx, y: ty };
-}
-
 function updatePlayer() {
-    if (player.input) {
-        if (player.input.left) player.position.x += 0.1;
-        if (player.input.right) player.position.x -= 0.1;
+    if (game.input.left) {
+        if (player.position.x > -10) player.position.x -= 0.1;
+        player.rotation.y = 0.3;
+    } else if (game.input.right) {
+        if (player.position.x < 10) player.position.x += 0.1;
+        player.rotation.y = -0.3;
+    } else {
+        player.rotation.y = 0;
     }
 }
 
@@ -204,45 +170,35 @@ function procGenerateRocks() {
     if (newTime - oldTime > 2000) {
         oldTime = new Date().getTime();
 
-        var spawnNum = Math.round(Math.random() * 3);
+        var spawnNum = Math.round(Math.random() * 10 * game.spawnRate);
         var spawnedObs;
         for (var i = 0; i < spawnNum; i++) {
-            spawnedObs = new Rock();
-            spawnedObs.mesh.position.x = -10 + Math.random() * 20;
-            spawnedObs.mesh.position.z = -20 - Math.random() * 10;
-            spawnedObs.mesh.scale.set(0.1, 0.1, 0.1);
+            spawnedObs = obstacleTypes[Math.floor(obstacleTypes.length * Math.random())];
+            spawnedObs.position.x = -20 + Math.random() * 30;
+            spawnedObs.position.z = -20;
+            spawnedObs.scale.set(1, 1, 1);
+            scene.add(spawnedObs);
             obstacles.push(spawnedObs);
-            scene.add(spawnedObs.mesh);
         }
 
-        var spawnNum = Math.round(Math.random() * 3);
-        var spawnedObs;
+        spawnNum = Math.round(Math.random() * 3 * game.spawnRate);
         for (var i = 0; i < spawnNum; i++) {
             spawnedObs = new Coin();
             spawnedObs.mesh.position.x = -10 + Math.random() * 20;
             spawnedObs.mesh.position.z = -20 - Math.random() * 10;
-            spawnedObs.mesh.scale.set(0.1, 0.1, 0.1);
+            spawnedObs.mesh.position.y = 0.3;
+            spawnedObs.mesh.scale.set(0.05, 0.05, 0.05);
             coins.push(spawnedObs);
             scene.add(spawnedObs.mesh);
         }
     }
 }
 
-
-// function procGenerateCoins() {
-//     newTime = new Date().getTime();
-
-//     if (newTime - oldTime > 2000) {
-//         oldTime = new Date().getTime();
-
-//     }
-// }
-
 function moveObstacles() {
     for (var i = 0; i < obstacles.length; i++) {
-        obstacles[i].mesh.position.z += 0.3;
-        if (obstacles[i].mesh.position.z > 6) {
-            scene.remove(obstacles[i].mesh);
+        obstacles[i].position.z += 0.1 * game.speed;
+        if (obstacles[i].position.z > 9) {
+            scene.remove(obstacles[i]);
             obstacles.splice(i, 1);
         }
     }
@@ -261,17 +217,18 @@ function moveCoins() {
 
 function detectCollision() {
     for (var i = 0; i < obstacles.length; i++) {
-        console.log(player.position.distanceTo(obstacles[i].mesh.position));
-        if (player.position.distanceTo(obstacles[i].mesh.position) < 3) {
-            scene.remove(obstacles[i].mesh);
+        if (player.position.distanceTo(obstacles[i].position) < 1) {
+            scene.remove(obstacles[i]);
             obstacles.splice(i, 1);
             initiateCollision("obstacle");
             game.finished = true;
+            var score = document.getElementById("score");
+            score.innerHTML = "You lost with " + game.points + " points: ";
         }
     }
 
     for (var i = 0; i < coins.length; i++) {
-        if (player.position.distanceTo(coins[i].mesh.position) < 3) {
+        if (player.position.distanceTo(coins[i].mesh.position) < 1) {
             scene.remove(coins[i].mesh);
             coins.splice(i, 1);
             initiateCollision("coin");
@@ -283,7 +240,8 @@ function initiateCollision(obj) {
     if (obj == "obstacle") console.log("Encountered obs");
     else if (obj == "coin") {
         game.points += 1;
-        console.log(game.points);
+        var score = document.getElementById("score");
+        score.innerHTML = game.points;
     }
 }
 
@@ -302,18 +260,73 @@ function Coin() {
     this.dist = 0;
 }
 
-function Rock() {
-    var geom = new THREE.TetrahedronGeometry(8, 2);
-    var mat = new THREE.MeshPhongMaterial({
-        color: Colors.brownDark,
-        shininess: 0,
-        specular: 0xffffff,
-        shading: THREE.FlatShading
+function loadObstacleTypes() {
+    var objLoader = new THREE.OBJLoader();
+    var mtlLoader = new THREE.MTLLoader();
+    objLoader.setPath('models/');
+    mtlLoader.setPath('models/');
+
+    mtlLoader.load('Oak_Dark_01.mtl', function(materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('Oak_Dark_01.obj', function(object) {
+            obstacleTypes.push(object);
+        });
     });
-    this.mesh = new THREE.Mesh(geom, mat);
-    this.mesh.scale.set(0.8, 0.8, 0.8);
-    this.mesh.castShadow = true;
-    this.angle = 0;
-    this.dist = 0;
-    //*/
+
+    mtlLoader.load('Tall_Rock_1_01.mtl', function(materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('Tall_Rock_1_01.obj', function(object) {
+            obstacleTypes.push(object);
+        });
+    });
+
+    mtlLoader.load('Tall_Rock_2_01.mtl', function(materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('Tall_Rock_2_01.obj', function(object) {
+            obstacleTypes.push(object);
+        });
+    });
+
+    mtlLoader.load('Tall_Rock_3_01.mtl', function(materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('Tall_Rock_3_01.mtl.obj', function(object) {
+            obstacleTypes.push(object);
+        });
+    });
+
+    mtlLoader.load('Oak_Fall_01.mtl', function(materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('Oak_Fall_01.obj', function(object) {
+            obstacleTypes.push(object);
+        });
+    });
+
+    mtlLoader.load('Oak_Green_01.mtl', function(materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('Oak_Green_01.obj', function(object) {
+            obstacleTypes.push(object);
+        });
+    });
+
+    mtlLoader.load('Tree_01.mtl', function(materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('Tree_01.obj', function(object) {
+            obstacleTypes.push(object);
+        });
+    });
+
+    mtlLoader.load('Tree_02.mtl', function(materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('Tree_02.obj', function(object) {
+            obstacleTypes.push(object);
+        });
+    });
 }
